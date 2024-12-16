39f940031b64b4f86944f1ab7fc62257fb86473f
Kaumijabdukarim <kaumijabdukarim@gmail.com>
2024-12-16 18:01:24 +0500

    123

--- js/app.js
+++ js/app.js
@@ -5,4 +5,21 @@ const burger = document.querySelector('.header__menu-burger');
         burger.addEventListener('click', () => {
             burger.classList.toggle('active');
             menu.classList.toggle('active');
-        });
+});
+const mainOfferSwiper = new Swiper(".swiper-container", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    loop: true,
+    speed: 5000,
+    navigation: {
+        nextEl: ".swiper-button-next", // Класс кнопки "вперед"
+        prevEl: ".swiper-button-prev", // Класс кнопки "назад"
+    },
+    pagination: {
+        el: ".swiper-pagination", // Класс контейнера пагинации
+        type: "fraction", // Пагинация в формате "цифры"
+    },
+});
+
+
+// 
